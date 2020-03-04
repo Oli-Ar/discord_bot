a355699a3585b6bd50c41b0767805cbfe1c2c6af
Oliver <oliver@archer.fi>
2020-03-04 16:59:57 +0000

    Messages are no longer deleted but just edited

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -3,13 +3,17 @@ const fs = module.require('fs');
 module.exports.run = async (bot, msg) => {
     fs.readFile('./userXP.json', 'utf8', async (err, res) => {
         if(err) console.log(err);
+        //FIXME: server checks
         let xpList = await (Object.values(JSON.parse(res)))
             .sort((l, r) => r['servers'][msg.guild.id].score - l['servers'][msg.guild.id].score);
         let pages = chunk(xpList, 10);
         pages.forEach((chunk, i)  => {
             pages[i] = chunk.map((obj, j) => `${i*10+j+1}. ${obj.name}: ${obj['servers'][msg.guild.id].score}`);
         });
-        swapPage(msg, 0, pages);
+        msg.channel.send("Loading Leaderboard...")
+            .then(sentMessage => {
+                swapPage(msg, sentMessage, 0, pages);
+            });
     });
 };
 
@@ -20,26 +24,27 @@ function chunk(array, size) {
     return [firstChunk].concat(chunk(array.slice(size, array.length), size));
 }
 
-function swapPage(msg, page, pages) {
+function swapPage(msg, sentMessage, page, pages) {
     const filter = (reaction, user) => {
         return ['⬅', '➡'].includes(reaction.emoji.name) && user.id === msg.author.id && !user.bot;
     };
 
-    msg.channel.send(`\`\`\`ml\n${msg.guild.name} Leaderboard:\n${pages[page].join('\n')}\nto see next page click the reaction arrows\`\`\``)
+    if(pages.length === 0) return msg.channel.send("Leaderboard Empty.");
+    sentMessage.edit(`\`\`\`ml\n${msg.guild.name} Leaderboard:\n${pages[page].join('\n')}\nto see next page click the reaction arrows\`\`\``)
         .then(message => {
             if(page !== 0) message.react('⬅');
             if(page !== pages.length-1) message.react('➡');
             message.awaitReactions(filter, {max: 1})
                 .then(reactions => {
                     if(reactions.first().emoji.name === '⬅' && page !== 0) {
-                        message.delete();
-                        return swapPage(msg, --page, pages);
+                        message.clearReactions();
+                        return swapPage(msg, message, --page, pages);
                     } else if(page !== page.length-1) {
-                        message.delete();
-                        return swapPage(msg, ++page, pages);
+                        message.clearReactions();
+                        return swapPage(msg, message, ++page, pages);
                     } else {
-                        message.delete();
-                        return swapPage(msg, page, pages)
+                        message.clearReactions();
+                        return swapPage(msg, message, page, pages);
                     }
                 }).catch(err => console.log(err));
         });
