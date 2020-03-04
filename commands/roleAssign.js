58ed6bd18c3018918b9a13607b1d82f614336d30
Oliver <oliver@archer.fi>
2020-03-04 21:07:07 +0000

    Rewrote function to make it actually work

--- commands/roleAssign.js
+++ commands/roleAssign.js
@@ -1,43 +1,36 @@
 const fs = module.require('fs');
+const { promisify } = module.require('util');
+const readFile = promisify(fs.readFile);
 
 module.exports.run = async (bot) => {
     console.log("Resetting roles");
-    fs.readFile('./userXP.json', 'utf8', async (xpErr, xpRes) => {
-        if(xpErr) console.log(xpErr);
-        fs.readFile('./roleToAssign.json', 'utf8', async (roleErr, roleRes) => {
-            if(roleErr) console.log(roleErr);
-            let rolesList = JSON.parse(roleRes);
-            rolesList.forEach(object => {
-                let server = bot.guilds.get(object.serverID);
-                let role = server.roles.get(object.roleID);
-                server.members.forEach(member => {
-                    if(!member.roles.get(object.roleID)) return;
-                    member.removeRole(role.id);
-                });
-
-                let i = 0; let j = 10;
-                let userList = (Object.values(JSON.parse(xpRes)))
-                    .sort((l, r) => r['servers'][server.id].score - l['servers'][server.id].score);
-                for(let value of userList) {
-                    if(!rolesList[value.server]) break;
-                    let guild = bot.guilds.get(value.server);
-                    let member = guild.members.get(value.id);
-                    let role = guild.roles.get(rolesList[value.server]['roleID']);
-                    if (member.permissions.has("BAN_MEMBERS") && i <= 10) {
-                        member.addRole(role);
-                    } else if (!member.permissions.has("BAN_MEMBERS") && j <= 10) {
-                        member.addRole(role);
-                        j++
-                    } else if(j > 10) {
-                        break;
-                    }
-                    i++
-                }
-            });
-        });
+    let servers = JSON.parse(await readFile('./roleToAssign.json'));
+    let users = JSON.parse(await readFile('./userXP.json'));
+    Object.values(servers).forEach(server => {
+        let sortedUserList = (Object.values(users))
+            .filter(o => o['servers'][server.serverID])
+            .sort((l, r) => r['servers'][server.serverID].score - l['servers'][server.serverID].score);
+        addRoles(bot, sortedUserList, servers, server, 0, 0);
     });
-    fs.writeFile('./userXP.json', {});
+
+    fs.writeFile('./userXP.json', JSON.stringify({}), err => {if(err) console.log(err);});
 };
+
+function addRoles(bot, userList, rolesList, server, i, j) {
+    if(userList.length <= 0) return;
+    for(const user of userList) {
+        if (j > 10) break;
+        let guild = bot.guilds.get(server.serverID);
+        let member = guild.members.get(user.id);
+        let role = guild.roles.get(rolesList[server.serverID]['roleID']);
+        if (member.permissions.has("BAN_MEMBERS") && i <= 10) {
+            member.addRole(role);
+        } else if (!member.permissions.has("BAN_MEMBERS") && j <= 10) {
+            member.addRole(role); j++
+        }
+        i++
+    }
+}
 
 module.exports.help = {
     "title": 'roleassign',
