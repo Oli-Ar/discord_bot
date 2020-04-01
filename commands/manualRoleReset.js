c031eeaefb54b9cdd26afac5ec5f15ea9f2734ff
Oliver <oliver@archer.fi>
2020-04-01 16:01:28 +0100

    Added checks to ensure only admins can reset the leaderboard

--- commands/manualRoleReset.js
+++ commands/manualRoleReset.js
@@ -1,6 +1,7 @@
 const roleAssign = require('./roleAssign');
 
 module.exports.run = async (bot, msg) => {
+    if(!msg.member.permissions.has('ADMINISTRATOR')) return;
     await roleAssign.run(bot);
     msg.channel.send("Manually reset roles and assigned new top 10 roles");
 };
