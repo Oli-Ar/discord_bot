4929013876267d9346d5156f791b24f62b4e61fb
Oliver <oliver@archer.fi>
2020-04-07 12:00:51 +0100

    Improved argument parsing as well as added extra checks to ensure a role isn't applied from more than one message

--- commands/reactAutoRole.js
+++ commands/reactAutoRole.js
@@ -1,6 +1,7 @@
 const fs = module.require('fs');
 
 module.exports.run = async (bot, msg) => {
+    console.log(msg.reactions);
     if(!msg.member.permissions.has('BAN_MEMBERS')) return;
     let msgArray = msg.content.split(" ");
     const validArgs = ['message', 'role-to-assign', 'channel', 'role-req', 'delete'];
@@ -9,58 +10,45 @@ module.exports.run = async (bot, msg) => {
         return result;
     }, {});
     let passedArgs = await Object.keys(argPos);
-    let msgToSend; let roleToAssign; let channel;
-    if(passedArgs.includes('channel')) {
-        channel = msg.guild.channels.cache.find(name => name.name.toLowerCase() === msgArray[argPos.channel].toLowerCase());
-    }
-    if(passedArgs[passedArgs.length-1] === 'message') { msgToSend = msgArray.slice(argPos.message).join(" ") }
-    else { msgToSend = msgArray.slice(argPos.message, argPos[passedArgs[passedArgs.indexOf('message')+1]]-1).join(" "); }
-    if(passedArgs[passedArgs.length-1] === 'role-to-assign') {
-        roleToAssign = msg.guild.roles.cache.find(name => name.name.toLowerCase() === msgArray.slice(argPos['role-to-assign']).join(" ").toLowerCase());
-    } else {
-        let indexEndArg = argPos[passedArgs[passedArgs.indexOf('role-to-assign')+1]]-1;
-        roleToAssign = msg.guild.roles.cache
-            .find(name => name.name.toLowerCase() === msgArray
-            .slice(argPos['role-to-assign'], indexEndArg).join(" ").toLowerCase());
-    }
-    let roleReq = undefined;
-    if(passedArgs[passedArgs.length-1] === 'role-req') {
-        roleReq =  msgArray.slice(argPos['roleReq']).join(" ");
-    } else if(passedArgs.includes('role-req')) {
-        let indexEndArg = argPos[passedArgs[passedArgs.indexOf('role-req')+1]]-1;
-        roleReq = msgArray.slice(argPos['role-req'], indexEndArg).join(" ").toLowerCase();
-    }
+    let msgToSend = parseArgs(passedArgs, argPos, msgArray, 'message');
+    let roleToAssign = msg.guild.roles.cache.find(r => r.name.toLowerCase() === parseArgs(passedArgs, argPos, msgArray, 'role-to-assign').toLowerCase());
+    let channel = msg.guild.channels.cache.find(c => c.name === parseArgs(passedArgs, argPos, msgArray, 'channel'));
+    let roleReq = parseArgs(passedArgs, argPos, msgArray, 'role-req');
     if(roleReq) {
         roleReq = roleReq.split(",").map(e => e.trim());
         roleReq = roleReq.map(e => msg.guild.roles.cache.find(r => r.name.toLowerCase() === e));
     }
     if(msgToSend && roleToAssign && channel) {
-        let requiredRoles = roleReq.length > 1 ? roleReq.map(e => e.name).slice(0, roleReq.length-1).join(", ")+" or "+roleReq[roleReq.length-1].name : roleReq[0].name;
-        channel.send(msgToSend + (roleReq ? ` (Requires ${requiredRoles})` : '')).then(sentMessage => {
-            sentMessage.react('☑');
-            const newMsgData = {
-                "msgID": sentMessage.id.toString(),
-                "channelID": channel.id.toString(),
-                "roleID": roleToAssign.id.toString(),
-                "reqRoleID": roleReq ? roleReq.map(e => e.id.toString()) : undefined,
-                "serverID": msg.guild.id.toString()
-            };
-            fs.readFile('./autoRoles.json', 'utf8', async (err, res) => {
-                if(err) console.log(err);
-                let allMsgData = JSON.parse(res);
-                allMsgData[roleToAssign.id] = newMsgData;
+        fs.readFile('./autoRoles.json', 'utf8', async (err, res) => {
+            if (err) console.log(err);
+            let allMsgData = JSON.parse(res);
+            if(allMsgData[roleToAssign.id.toString()]) {
+                return msg.channel.send("There's an existing message assigning this role.");
+            }
+            let requiredRoles = roleReq.length > 1 ? roleReq.map(e => e.name).slice(0, roleReq.length - 1).join(", ") + " or " + roleReq[roleReq.length - 1].name : roleReq[0].name;
+            channel.send(msgToSend + (roleReq ? ` (Requires ${requiredRoles})` : '')).then(sentMessage => {
+                sentMessage.react('☑');
+                allMsgData[roleToAssign.id] = {
+                    "msgID": sentMessage.id.toString(),
+                    "channelID": channel.id.toString(),
+                    "roleID": roleToAssign.id.toString(),
+                    "reqRoleID": roleReq ? roleReq.map(e => e.id.toString()) : undefined,
+                    "serverID": msg.guild.id.toString()
+                };
                 fs.writeFile('./autoRoles.json', JSON.stringify(allMsgData, null, 4), async err => {
-                    if(err) console.log(err);
+                    if (err) console.log(err);
                 });
             });
         });
     } else if(passedArgs.includes('delete') && roleToAssign) {
-        let roleToDelete = msg.guild.roles.cache.find(name => name === roleToAssign);
         fs.readFile('./autoRoles.json', 'utf8', async (err, res) => {
             if(err) console.log(err);
             let savedMessages = JSON.parse(res);
-            let messageToDelete = savedMessages[roleToDelete.id.toString()];
-            delete savedMessages[roleToDelete.id.toString()];
+            if(!savedMessages[roleToAssign.id.toString()]) {
+                return msg.channel.send("There is no message assigning this role.");
+            }
+            let messageToDelete = savedMessages[roleToAssign.id.toString()];
+            delete savedMessages[roleToAssign.id.toString()];
             fs.writeFile('./autoRoles.json', JSON.stringify(savedMessages, null, 4), async err => {
                 if(err) console.log(err);
             });
@@ -71,6 +59,18 @@ module.exports.run = async (bot, msg) => {
         });
     } else {
         return msg.channel.send("A valid channel, role and message are required, or if removing message a valid role is required");
+    }
+};
+
+const parseArgs = (argsList, argsPos, messageContent, arg) => {
+    if(!argsList.includes(arg)) return undefined;
+    if(argsList[argsList.length-1] === arg) {
+        return messageContent.slice(argsPos[arg]).join(" ");
+    } else {
+        let argIndexEnd = argsPos[argsList[argsList.indexOf(arg)+1]]-1;
+        let argumentList = messageContent.slice(argsPos[arg], argIndexEnd);
+        if(arg === 'message') return argumentList.join(" ");
+        return argumentList.join(" ").toLowerCase();
     }
 };
 
