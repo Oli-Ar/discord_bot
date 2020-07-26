c9b479e62a5cbacec9b021065747d74536500b50
Oliver <oliver@archer.fi>
2020-07-26 16:10:59 +0100

    moved functions used by more than command to seperate files

---
+++ commands/helperFunctions/fetchPartial.js
@@ -0,0 +1,10 @@
+module.exports.fetch = async reaction => {
+    reaction = new Promise((resolve, reject) => {
+        if(reaction.partial) {
+            reaction.fetch().then(async nonPartial => {
+                resolve(nonPartial);
+            }).catch(e => { console.log(e); reject(e); });
+        } else { resolve(reaction); }
+    });
+    return reaction;
+};
---
+++ commands/helperFunctions/parseArgs.js
@@ -0,0 +1,12 @@
+module.exports.parse = (argsList, argsPos, messageContent, arg) => {
+    if(!argsList.includes(arg)) return "";
+    if(argsList[argsList.length-1] === arg) {
+        return messageContent.slice(argsPos[arg]).join(" ");
+    } else {
+        let argIndexEnd = argsPos[argsList[argsList.indexOf(arg)+1]]-1;
+        let argumentList = messageContent.slice(argsPos[arg], argIndexEnd);
+        if(arg === 'message') return argumentList.join(" ");
+        return argumentList.join(" ").toLowerCase();
+    }
+
+};
