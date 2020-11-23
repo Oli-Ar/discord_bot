55f51d22af25f380f8750df9decaa96f5d6c20e4
Oliver Archer <oliver@archer.fi>
2020-11-23 16:55:12 +0000

    Finished leaderboard command, now displays embed with leaderboard with pages that can turned with arrow reactions

--- src/commands/leaderboard.rs
+++ src/commands/leaderboard.rs
@@ -1,7 +1,7 @@
 use crate::PostgresPool;
 
 use serenity::client::Context;
-use serenity::collector::reaction_collector::ReactionAction;
+use serenity::collector::reaction_collector::ReactionAction::Added;
 use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::model::channel::{Message, ReactionType};
 
@@ -16,8 +16,16 @@ struct UserList {
 
 #[command]
 pub async fn leaderboard(ctx: &Context, msg: &Message) -> CommandResult {
-    let data = ctx.data.write().await;
-    let pool = data.get::<PostgresPool>().unwrap();
+    let sent_message = msg.channel_id.say(ctx, "Fetching leaderboard...").await?;
+
+    let pool = ctx
+        .data
+        .read()
+        .await
+        .get::<PostgresPool>()
+        .cloned()
+        .unwrap();
+
     let rows: Vec<UserList> = sqlx::query_as!(
         UserList,
         "
@@ -28,7 +36,7 @@ pub async fn leaderboard(ctx: &Context, msg: &Message) -> CommandResult {
         ",
         sqlx::types::BigDecimal::from_u64(msg.guild_id.unwrap().0).unwrap(),
     )
-    .fetch_all(pool)
+    .fetch_all(&pool)
     .await
     .unwrap();
 
@@ -36,8 +44,6 @@ pub async fn leaderboard(ctx: &Context, msg: &Message) -> CommandResult {
         .chunks(10)
         .map(|c| c.to_vec())
         .collect::<Vec<Vec<UserList>>>();
-
-    let sent_message = msg.channel_id.say(ctx, "Beep").await?;
 
     let mut joined_pages: Vec<String> = Vec::new();
     let mut all_pages_stream = futures::stream::iter(row_chunks.into_iter().enumerate());
@@ -58,30 +64,84 @@ pub async fn leaderboard(ctx: &Context, msg: &Message) -> CommandResult {
         }
     }
 
-    display_board(ctx, sent_message, 0 as usize, &joined_pages).await?;
+    let guild_name = ctx.http.get_guild(msg.guild_id.unwrap().0).await?.name;
+    display_board(ctx, sent_message, 0 as usize, &joined_pages, guild_name).await?;
 
     Ok(())
 }
 
-#[allow(unused_variables)]
+#[allow(unused_variables, unused_mut)]
 async fn display_board(
     ctx: &Context,
     mut sent_message: Message,
     mut page: usize,
     pages: &Vec<String>,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    sent_message.edit(ctx, |m| m.content("boop")).await?;
-    let reaction = sent_message
-        .await_reaction(ctx)
-        .timeout(core::time::Duration::from_secs(120))
-        // .filter(|r| {
-        //     return r.emoji == ReactionType::Unicode("▶\u{fe0f}")
-        //         || r.emoji == ReactionType::Unicode('\u{25c0}'.to_string());
-        // })
-        .message_id(sent_message.id)
-        .await;
+    guild_name: String,
+) -> CommandResult {
+    loop {
+        sent_message
+            .edit(ctx, |m| {
+                m.embed(|e| {
+                    e.title(format!("{} Leaderboard", guild_name))
+                        .description(format!("```md\n{}```", &pages[page]))
+                        .footer(|f| {
+                            f.text(format!(
+                                "{}/{} ● Click arrow to see next page",
+                                page + 1,
+                                pages.len()
+                            ))
+                        })
+                        .color((235, 64, 52))
+                })
+                .content("")
+            })
+            .await?;
+        if page != 0 {
+            sent_message
+                .react(&ctx.http, ReactionType::Unicode(String::from("◀\u{fe0f}")))
+                .await?;
+        }
+        if page != pages.len() - 1 {
+            sent_message
+                .react(&ctx.http, ReactionType::Unicode(String::from("▶\u{fe0f}")))
+                .await?;
+        }
+        let reaction = sent_message
+            .await_reaction(ctx)
+            .timeout(core::time::Duration::from_secs(120))
+            .filter(move |r| {
+                return r.emoji == ReactionType::Unicode(String::from("▶\u{fe0f}"))
+                    || r.emoji == ReactionType::Unicode(String::from("◀\u{fe0f}"));
+            })
+            .await;
 
-    println!("{:?}", reaction);
+        match reaction {
+            Some(ra) => {
+                if let Added(r) = &*ra {
+                    if r.emoji == ReactionType::Unicode(String::from("▶\u{fe0f}"))
+                        && page < pages.len() - 1
+                    {
+                        page += 1;
+                    } else if r.emoji == ReactionType::Unicode(String::from("◀\u{fe0f}"))
+                        && page > 0
+                    {
+                        page -= 1;
+                    }
+                    sent_message.delete_reactions(&ctx.http).await?;
+                }
+            }
+            _ => {
+                sent_message.delete_reactions(&ctx.http).await?;
+                sent_message
+                    .edit(&ctx.http, |m| {
+                        m.content("Leaderboard expired use command again to view.")
+                            .suppress_embeds(true)
+                    })
+                    .await?;
+                break;
+            }
+        }
+    }
 
     Ok(())
 }
